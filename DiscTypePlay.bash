0bf5b6d0cbc1ace0264d598a75b9342a4557d953
littlejeem <littlejeem@users.noreply.github.com>
2015-04-22 22:44:55 +0100

    Update DiscTypePlay.bash
    added comment to show logic

--- DiscTypePlay.bash
+++ DiscTypePlay.bash
@@ -1,20 +1,40 @@
 #!/bin/bash
 # code below is derived from work by JimVanns, thanks
 # https://github.com/jvanns/htpc/blob/master/dsc-trg-q
+#
+#
+##########################################################################################################################
+##                                      DEFINE VARIABLES HERE                                                           ##
+## $HOME DOES NOT NEED DEFINING AS IT SEEMS TO BE BUILT INTO BASH FROM WHAT I HAVE READ                                 ##
+## POST RELATING TO USING WHAT I THINK ARE UDEV ENVIRONMENTAL VARIABLES E.G. $ID_CDROM_MEDIA_CD DO NOT APPEAR TO NEED   ##
+## DEFINING THEMSELVES.....ALSO PART OF BASH OR LINUX COMMAND STRUCTURE CALLED BY BASH??                                ##
+##########################################################################################################################
+#
+#
 MEDIA=
+#
+#
+##########################################################################################################
+## LEFT IN SO AS TO ALTER AS LITTLE AS POSSIBLE, I HAVE READ THAT DELAYS OFTEN IRON OUT KINKS IN CODE,  ##
+## PLUS ALSO FOUND IT USEFULL TO ALLOW A SMALL DELAY FOR CD-DRIVE TO DO ITS THING AFTER PUTTING DISK IN ##
+##########################################################################################################
+#
+#
 sleep 2
-
+#
+#
+#
 if [ "$ID_CDROM_MEDIA_BD" = "1" ]
 then
         MEDIA=bluray
         (
-        echo "$MEDIA" >> /jlivin25/home/myscripts/scriptlogs/DiscTypePlay.log
+        echo "MEDIA" >> /$HOME/myscripts/scriptlogs/DiscTypePlay.log
         )
 elif [ "$ID_CDROM_MEDIA_DVD" = "1" ]
 then
         MEDIA=dvd
         (
-        echo "$MEDIA" >> /jlivin25/home/myscripts/scriptlogs/DiscTypePlay.log
+        echo "$MEDIA" >> /$HOME/myscripts/scriptlogs/DiscTypePlay.log
         )
 elif [ "$ID_CDROM_MEDIA_CD" = "1" ]
 then
@@ -22,6 +42,6 @@ then
 #       NO_EJECT=1
         MEDIA=cdrom
         (
-        echo "$MEDIA" >> /jlivin25/home/myscripts/scriptlogs/DiscTypePlay.log
+        echo "$MEDIA" >> /$HOME/myscripts/scriptlogs/DiscTypePlay.log
         )
 fi
